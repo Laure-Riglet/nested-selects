fd0a231dd205b3b4c28d4ec4cbf7f8f20377f15b
Laure-Riglet <laure.riglet@gmail.com>
2023-04-20 21:17:28 +0200

    ok

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CATEGORY SELECT WITH UNLIMITED DEPTH SUBCATEGORIES
+# Category cascade selects
 
 If a category has subcategories, the category select will show a new select with the subcategories.
 If the subcategory has subcategories, the category select will show a new select with its subcategories.
--- assets/js/select.js
+++ assets/js/select.js
@@ -1,5 +1,4 @@
 const select = {
-    env: "prod",
     selectContainer: document.querySelector('#select-container'),
     selectTemplate: document.querySelector('#select-template'),
     optionTemplate: document.querySelector('#option-template'),
@@ -13,79 +12,87 @@ const select = {
         select.createSelect(categories);
     },
 
+    /**
+     *  API CALLS
+     */
+
     fetchAllCategories: async function () {
-        if (select.env === "dev") {
-            const response = await fetch('./js-based/data.json');
-            if (!response.ok) {
-                return;
-            }
-            const data = await response.json();
-            return data;
-        } else {
-            const response = await fetch('https://l4ur3l3i.mykreezalid.com/front_api/v1/categories');
-            if (!response.ok) {
-                return;
-            }
-            const data = await response.json();
-            const categories = data.categories;
-            return categories;
+        const response = await fetch('https://l4ur3l3i.mykreezalid.com/front_api/v1/categories');
+        if (!response.ok) {
+            return;
         }
+        const data = await response.json();
+        const categories = data.categories;
+        return categories;
     },
 
     fetchCategory: async function (categoryId) {
-        if (select.env === "dev") {
-            const response = await fetch('./js-based/data.json');
-            if (!response.ok) {
-                return;
-            }
-            const data = await response.json();
-            return data;
-        } else {
-            const response = await fetch('https://l4ur3l3i.mykreezalid.com/front_api/v1/categories/' + categoryId + '/');
-            if (!response.ok) {
-                return;
-            }
-            const data = await response.json();
-            const category = data.category;
-            return category;
+        const response = await fetch('https://l4ur3l3i.mykreezalid.com/front_api/v1/categories/' + categoryId + '/');
+        if (!response.ok) {
+            return;
         }
+        const data = await response.json();
+        const category = data.category;
+        return category;
     },
 
+    /**
+     *  DOM MANIPULATIONS
+     * */
+
     createSelect: function (categories) {
+
         select.currentDepth += 1;
+
+        // Duplicate label and select template
         const selectClone = select.selectTemplate.content.cloneNode(true);
+
+        // Set label and select attributes
         const label = selectClone.querySelector('label');
-        console.log(label);
         label.htmlFor = 'category-lvl-' + select.currentDepth;
         label.dataset.depth = select.currentDepth;
         label.ariaLabel = 'Category select level ' + select.currentDepth;
+
         const selectField = selectClone.querySelector('select');
         console.log(selectClone, selectField);
         selectField.name = 'category-lvl-' + select.currentDepth;
         selectField.id = 'category-lvl-' + select.currentDepth;
 
+        // Create options
         Object.values(categories).forEach(category => {
             const option = select.createOption(category);
             selectField.appendChild(option);
         });
 
+        // Add event listener on select change
         selectField.addEventListener('change', select.handleSelectChange);
+
+        // Append select to the select container in the DOM
         select.selectContainer.appendChild(selectClone);
     },
 
     createOption: function (category) {
+
+        // Duplicate option template
         const optionClone = select.optionTemplate.content.cloneNode(true);
+
+        // Select and set option attributes
         const option = optionClone.querySelector('option');
         option.value = category.id;
         option.textContent = category.title;
         option.dataset.id = category.id;
+
         return option;
     },
 
     handleSelectChange: async function (event) {
+
         const selectField = event.target;
-        // Remove all selects after the current one
+
+        // Get the depth of the current select field
         const labelFieldDepth = selectField.previousElementSibling.dataset.depth;
+
+        // Remove all select fields and labels with a depth higher than the current select field
         const labels = document.querySelectorAll('label');
         labels.forEach(label => {
             if (label.dataset.depth > labelFieldDepth) {
@@ -94,8 +101,14 @@ const select = {
 
             }
         });
+
+        // Update current depth in the counter
         select.currentDepth = parseInt(labelFieldDepth);
+
+        // Get the category object from the selected option
         const category = await select.fetchCategory(selectField.value);
+
+        // Create a new select field if the category has children
         if (category.child_count > 0) {
             select.createSelect(category.children);
         }
